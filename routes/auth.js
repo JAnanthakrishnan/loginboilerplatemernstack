ef572797ac8c70b8c0a296bc1bb894afaa33815b
J Ananthakrishnan <ananthakrishnan2017@gmail.com>
2020-08-15 22:17:50 +0530

    Integrating with front-end

--- routes/auth.js
+++ routes/auth.js
@@ -5,11 +5,18 @@ const { check, validationResult } = require('express-validator');
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const User = require('../models/User');
+const auth = require('../middleware/auth');
 //@route GET api/auth
 //@desc Get logged in user
 //@access Private
-router.get('/', (req, res) => {
-  res.send('Get logged in user');
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json({ user });
+  } catch (err) {
+    console.error(err.messsage);
+    return res.status(500).send('Server Error...');
+  }
 });
 //@route POST api/auth
 //@desc Auth user and get token
