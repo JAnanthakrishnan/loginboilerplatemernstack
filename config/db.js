62b30d3cca47453fd221e1cd41b9b0864082080c
J Ananthakrishnan <ananthakrishnan2017@gmail.com>
2020-08-15 20:25:01 +0530

    Login and registration done

--- config/db.js
+++ config/db.js
@@ -12,6 +12,7 @@ const connectDB = async () => {
     console.log("MongoDB Connected...");
   } catch (err) {
     console.error(err.message);
+    process.exit(1);
   }
 };
 module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
+//Connect database
 connectDB();
+//Init middleware
+app.use(express.json({ extended: false }));
 app.get("/", (req, res) =>
   res.json({ msg: "Welcome to the login boiler plate" })
 );
